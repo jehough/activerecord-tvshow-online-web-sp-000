649fba3166b371b008449dbe7f1de9498038456d
Jim Hough <teachlife@outlook.com>
2019-06-03 02:28:45 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,2 +1,9 @@
 class Show < ActiveRecord::Base
+  def self.highest_rating
+    Show.maximum('rating')
+  end
+
+  def self.most_popular_show
+    popular = highest_rating
+    Show.find_by(:rating = 10)
 end
